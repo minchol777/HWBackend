5993d95e26723ee2e35c14541b17d66c9138f706
seo <smc991009@gmail.com>
2023-06-06 13:11:02 +0900

    0606 1.01

--- src/main/java/com/homework/HWBackend/controller/PointsController.java
+++ src/main/java/com/homework/HWBackend/controller/PointsController.java
@@ -2,9 +2,11 @@ package com.homework.HWBackend.controller;
 
 
 import com.homework.HWBackend.DTO.PointsDTO;
+import com.homework.HWBackend.model.Comments;
 import com.homework.HWBackend.model.Points;
 import com.homework.HWBackend.model.Post;
 import com.homework.HWBackend.model.Users;
+import com.homework.HWBackend.repository.CommentsRepository;
 import com.homework.HWBackend.repository.PointsRepository;
 import com.homework.HWBackend.repository.PostRepository;
 import com.homework.HWBackend.repository.UsersRepository;
@@ -29,46 +31,57 @@ public class PointsController {
 
     @Autowired
     private PostRepository postRepository;
-    
+
+    @Autowired
+    private CommentsRepository commentsRepository;
+
     // 포인트 획득 
     @PutMapping("/add")
-    public Points updatePoint(@RequestBody PointsDTO pointsDTO){
-        Users users = usersRepository.findById(pointsDTO.getId()).orElseThrow(()->
+    public Points updatePoint(@RequestBody PointsDTO pointsDTO) {
+        Users users = usersRepository.findById(pointsDTO.getId()).orElseThrow(() ->
                 new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 user id"));
         Points points = pointsRepository.findByUserid(users);
         if (points == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "점수 정보가 없는 사용자");
         }
         int p = points.getPoint();
-        p = p+(pointsDTO.getPoint());
+        p = p + (pointsDTO.getPoint());
 
         points.setPoint(p);
 
         return pointsRepository.save(points);
     }
 
-    
+
     // 포인트 사용
     @DeleteMapping("/delete/{postid}")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteByPoint(@PathVariable int postid, @RequestBody PointsDTO pointsDTO){
-        Users users = usersRepository.findById(pointsDTO.getId()).orElseThrow(()->
+    public void deleteByPoint(@PathVariable int postid, @RequestBody PointsDTO pointsDTO) {
+        Users users = usersRepository.findById(pointsDTO.getId()).orElseThrow(() ->
                 new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 user id"));
         Points points = pointsRepository.findByUserid(users);
         if (points == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "점수 정보가 없는 사용자");
         }
         int p = points.getPoint();
-        if (p<NEED_POINT){
+        if (p < NEED_POINT) {
             throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, "포인트가 부족합니다");
-        }else{
-            p=p-100;
+        } else {
+            p = p - 100;
             points.setPoint(p);
-            pointsRepository.save(points);
         }
 
 
-        postRepository.deleteById(postid);
+        Post post = postRepository.findById(postid).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "post_id에 해당하는 게시글이 없습니다."));
+        pointsRepository.save(points);
+        List<Comments> comments = commentsRepository.findByPostid(post);
+        for (Comments comment : comments) {
+            commentsRepository.delete(comment);
+        }
+
+        postRepository.delete(post);
+        ;
     }
 
 }
